66555e52afedee89e4ff8a1ab42575e81ce9577e
Yash Mandloi <yashmandloi23@gmail.com>
2025-12-08 19:28:51 +0530

    routing done

---
+++ routing/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import User from './components/User.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/user/:id' element={<User/>}/>
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
